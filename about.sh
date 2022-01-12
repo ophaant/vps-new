df4b0b1bf53c40960bb2e561471d1bfb6eeb934f
vilfanvenci@gmail.com <vilfanvenci@gmail.com>
2022-01-12 16:47:34 +0700

    Update Warna Terminal

--- about.sh
+++ about.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
 clear
-echo -e "================================================="
-echo -e "# Script Auto Install SSH & OpenVPN             #"
-echo -e "#-----------------------------------------------#"
+m="\033[0;1;36m"
+y="\033[0;1;37m"
+yy="\033[0;1;31m"
+m1="\033[1;34m"
+echo -e "=================================================" | lolcat
+echo -e "$m1# Script Auto Install SSH & OpenVPN             #"
+echo -e "#-----------------------------------------------#" | lolcat
 echo -e "# For Debian 9 & 10 Buster 64 bit               #"
 echo -e "# For VPS with KVM and VMWare virtualization    #"
-echo -e "# Modified by PT. Inovasi Bakti Nusantara                       #"
-echo -e "#-----------------------------------------------#"
+echo -e "# Modified by PT. Inovasi Bakti Nusantara       #"
+echo -e "#-----------------------------------------------#" | lolcat
 echo -e "# Thanks To                                     #"
-echo -e "#-----------------------------------------------#"
-echo -e "# PT. Inovasi Bakti Nusantara                                   #"
+echo -e "#-----------------------------------------------#" | lolcat
+echo -e "# PT. Inovasi Bakti Nusantara                   #"
 echo -e "# My Family                                     #"
 echo -e "# Google                                        #"
 echo -e "# REDESGEEK.COM                                 #"
 echo -e "#                                               #"
-echo -e "#-----------------------------------------------#"
-echo -e "================================================="
+echo -e "#-----------------------------------------------#" | lolcat
+echo -e "=================================================" | lolcat
